ae65f4ba83a619318d7509c3a067d65406566c03
Rakshita <bhattrakshita13@gmail.com>
2023-08-22 21:54:42 +0530

    Navbar Modified

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -2,9 +2,17 @@ import React, {useState} from 'react'
 import "./navbar.css";
 import { RiMenu3Line, RiCloseLine } from 'react-icons/ri'
 
+const Menu = ()  =>(
+  <>
+  <p><a href='#home'>Home</a></p>
+  <p><a href='#wia'>What is InnovAI?</a></p>
+  <p><a href='#possibility'>Open AI</a></p>
+  <p><a href='features'>Case Studies</a></p>
+  <p><a href='blog'>Library</a></p>
+  </>
+)
 
-
-const navbar = () => {
+const Navbar = () => {
   const [toggleMenu, setToggleMenu] = useState(false);
   return (
     <div className='innovai__navbar'>
@@ -13,11 +21,7 @@ const navbar = () => {
       <h2>InnovAI</h2>
        </div>
        <div className='innovai__navbar-links_container'>
-        <p><a href='#home'>Home</a></p>
-        <p><a href='#wia'>What is InnovAI?</a></p>
-        <p><a href='#possibility'>Open AI</a></p>
-        <p><a href='features'>Case Studies</a></p>
-        <p><a href='blog'>Library</a></p>
+        <Menu/>
        </div>
       </div>
       <div className='innovai__navbar-sign'>
@@ -29,8 +33,19 @@ const navbar = () => {
         ? <RiCloseLine color='#fff' size={24} onClick={()=> setToggleMenu(false)}></RiCloseLine>
         : <RiMenu3Line color='#fff' size={24} onClick={()=> setToggleMenu(true)}></RiMenu3Line>
         }
-
-
+        {toggleMenu && (
+          <>
+          <div className='innovai__navbar-menu_container scale-up-center'></div>
+               <div className='innovai__navbar-menu_container-links'>
+                <Menu/>
+        <div className='innovai__navbar-menu_container-links'>
+          <p>Sign in</p>
+          <button type='button'><h3></h3>Sign up</button>
+          </div>
+            </div>
+            </>
+        )
+        }
         
       </div>
       </div>
@@ -38,4 +53,4 @@ const navbar = () => {
   )
 }
 
-export default navbar
+export default Navbar
